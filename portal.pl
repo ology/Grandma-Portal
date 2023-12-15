67df66b863d9aaa0757716c56f7d9b3e1e63d86a
Gene Boggs <gene@ology.net>
2023-12-15 10:46:02 -0800

    Initial commit

---
+++ portal.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/env perl
+use Mojolicious::Lite -signatures;
+
+use Capture::Tiny qw(capture);
+use Data::Dumper::Compact qw(ddc);
+
+get '/' => sub ($c) {
+  my $google = $c->param('google') // 0;
+  $c->render(
+    template => 'index',
+    google   => $google,
+  );
+} => 'index';
+
+app->log->level('info');
+
+app->start;
+
+__DATA__
+
+@@ index.html.ep
+% layout 'default';
+% title 'Portal';
+
+<a href="https://mail.google.com/mail/u/0/#inbox" class="btn btn-outline-dark" target="_blank"><i class="fa fa-cogs"></i> Gmail</a>
+<a href="https://www.google.com/" class="btn btn-outline-dark" target="_blank"><i class="fa fa-cogs"></i> Google</a>
+<a href="https://family.ology.net/" class="btn btn-outline-dark" target="_blank"><i class="fa fa-cogs"></i> Family</a>
+
+@@ layouts/default.html.ep
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/css/fontawesome.css" rel="stylesheet">
+    <link href="/css/solid.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+    <title><%= title %></title>
+    <link href="/css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <p></p>
+    <div class="container padpage faded rounded">
+      <!-- <h3><a href="/"><%= title %></a></h3> -->
+      <%= content %>
+<!--
+      <p></p>
+      <div id="footer" class="smalltext">
+        <hr>
+        Built by <a href="http://gene.ology.net/">Gene</a>
+        with <a href="https://www.perl.org/">Perl</a> and
+        <a href="https://mojolicious.org/">Mojolicious</a>
+        | <a href="/help">Help!</a>
+      </div>
+-->
+    </div>
+  </body>
+</html>
+
