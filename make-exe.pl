610d86f0eff49f1cb2a29a6bc3be8e9ee06a4aac
Gene Boggs <gene@ology.net>
2023-12-19 15:27:23 -0800

    Fix command

--- make-exe.pl
+++ make-exe.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-# nb: This is experimental so far...
+# nb: I thought this was needed, but it turns out not to be
 
 use strict;
 use warnings;
@@ -9,7 +9,7 @@ use File::Which qw(which);
 
 my $pp = which('pp');
 
-my @cmd = ($pp, qw(-g -o portal.exe portal.pl));
+my @cmd = ($pp, qw(-g -o run-portal.exe run-portal.pl));
 
 system(@cmd) == 0 or die "system(@cmd) failed: $?";
 
