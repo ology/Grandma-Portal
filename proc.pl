634ba34491330f5883f2ef70adb09825d254b6d2
Gene Boggs <gene@ology.net>
2023-12-20 20:24:29 -0800

    Simplify

--- proc.pl
+++ proc.pl
@@ -2,16 +2,12 @@
 use strict;
 use warnings;
 
+use List::Util qw(any);
 use Win32::Process::List ();
 
 my $proc = Win32::Process::List->new;
 
 my %list = $proc->GetProcesses; # hashes with PID and process name
 
-for my $key (keys %list) {
-      # $list{$key} is now the process name and $key is the PID
-      print sprintf "%30s has PID %15s\n", $list{$key}, $key;
-}
+print "Perl!\n" if any { $list{$_} =~ /^perl\.exe$/ } keys %list;
 
-my $pid = $proc->GetProcessPid('run-portal');
-warn __PACKAGE__,' L',__LINE__,' ',,"PID: $pid\n";
